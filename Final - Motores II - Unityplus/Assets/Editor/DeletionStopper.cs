c6f1f018ae086478e40be4e3c5427a1865885a59
j33zas <matias.levy@davinci.edu.ar>
2021-02-09 17:04:13 -0300

    XD

---
+++ Final - Motores II - Unityplus/Assets/Editor/DeletionStopper.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[ExecuteAlways()]
+public class DeletionStopper : UnityEditor.AssetModificationProcessor
+{
+    public FavList defaultList;
+    static AssetDeleteResult OnWillDeleteAsset(string path, RemoveAssetOptions opt)
+    {
+        FavList list = (FavList)AssetDatabase.LoadAssetAtPath(AssetDatabase.GetAssetPath(defaultList), typeof(FavList));
+        if (list.favs.Contains(AssetDatabase.LoadAssetAtPath(path, typeof(object))))
+        {
+            EditorUtility.DisplayDialog("Error", "You can't delete objects that are favourited,  remove it from favourites to delete it", "Thanks", null);
+            return AssetDeleteResult.FailedDelete;
+        }
+        else
+            return AssetDeleteResult.DidDelete;
+    }
+}
