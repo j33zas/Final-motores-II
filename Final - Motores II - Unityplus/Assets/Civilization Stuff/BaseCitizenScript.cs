0466f084233641c65de0eaa437deb16a5c221343
jojoshua-ohno <joshua.aguilarlynch@davinci.edu.ar>
2021-02-10 02:59:41 -0300

    cosas base para mi herramienta
     ni idea por qué no me deja asignarle el script al prefab, despues veo

---
+++ Final - Motores II - Unityplus/Assets/Civilization Stuff/BaseCitizenScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCitizenScript : MonoBehaviour
+{
+    public float LevelOfAggression;
+    public float LevelOfIntelligence;
+    public float LevelOfStrength;
+    public float LevelofAgility;
+    public float LevelOfPerception;
+
+    public GameObject head;
+    public GameObject RightHand;
+    public GameObject LeftHand;
+    public GameObject LeftEyebrow;
+    public GameObject RightEyebrow;
+    public GameObject Mouth;
+    public GameObject Eyes;
+    public GameObject EyesBase;
+    public GameObject LeftLeg;
+    public GameObject RightLeg;
+    public List<GameObject> Hats = new List<GameObject>();
+}
