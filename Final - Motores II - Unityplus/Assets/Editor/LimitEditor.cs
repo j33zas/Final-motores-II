02d9c141a93ef92f9a028396dbf304d668d85990
jojoshua-ohno <joshua.aguilarlynch@davinci.edu.ar>
2021-02-13 22:18:11 -0300

    Va quedando algo
    yes

--- Final - Motores II - Unityplus/Assets/Editor/LimitEditor.cs
+++ Final - Motores II - Unityplus/Assets/Editor/LimitEditor.cs
@@ -29,16 +29,28 @@ public class LimitEditor : Editor
         var v = EditorWindow.GetWindow<SceneView>().camera.pixelRect;
 
         var main = Camera.current.WorldToScreenPoint(_ld.transform.position);
+
         var tanA = Camera.current.WorldToScreenPoint(_ld.tangentA.transform.position);
         var tanB = Camera.current.WorldToScreenPoint(_ld.tangentB.transform.position);
+        var tanC = Camera.current.WorldToScreenPoint(_ld.tangentC.transform.position);
+        var tanD = Camera.current.WorldToScreenPoint(_ld.tangentD.transform.position);
+
         var baseA = Camera.current.WorldToScreenPoint(_ld.bezierA.transform.position);
         var baseB = Camera.current.WorldToScreenPoint(_ld.bezierB.transform.position);
+        var baseC = Camera.current.WorldToScreenPoint(_ld.bezierC.transform.position);
+        var baseD = Camera.current.WorldToScreenPoint(_ld.bezierD.transform.position);
 
         var mainRect = new Rect(main.x - 40, Screen.height - main.y - 100, 90, 40);
+
         var tangARect = new Rect(tanA.x - 40, Screen.height - tanA.y - 150, 75, 20);
         var tangBRect = new Rect(tanB.x - 40, Screen.height - tanB.y - 150, 75, 20);
+        var tangCRect = new Rect(tanC.x - 40, Screen.height - tanC.y - 150, 75, 20);
+        var tangDRect = new Rect(tanD.x - 40, Screen.height - tanD.y - 150, 75, 20);
+
         var baseARect = new Rect(baseA.x - 40, Screen.height - baseA.y - 150, 75, 20);
         var baseBRect = new Rect(baseB.x - 40, Screen.height - baseB.y - 150, 75, 20);
+        var baseCRect = new Rect(baseC.x - 40, Screen.height - baseC.y - 150, 75, 20);
+        var baseDRect = new Rect(baseD.x - 40, Screen.height - baseD.y - 150, 75, 20);
 
         if (GUI.Button(new Rect(v.width - 150, v.height - 70, 120, 50), "Return Tangent A"))
         {
@@ -92,8 +104,13 @@ public class LimitEditor : Editor
 
         GUI.TextArea(tangARect, "Tangent A");
         GUI.TextArea(tangBRect, "Tangent B");
+        GUI.TextArea(tangCRect, "Tangent C");
+        GUI.TextArea(tangDRect, "Tangent D");
+
         GUI.TextArea(baseARect, "Base A");
         GUI.TextArea(baseBRect, "Base B");
+        GUI.TextArea(baseCRect, "Base C");
+        GUI.TextArea(baseDRect, "Base D");
 
 
         //AmountPanel();
@@ -106,7 +123,7 @@ public class LimitEditor : Editor
                     new Vector3(tgt.transform.position.x +5f, tgt.transform.position.y, tgt.transform.position.z- 5f),
         };
 
-        if (tgt.tangentA && tgt.tangentB && tgt.bezierA && tgt.bezierB)
+        if (tgt.tangentA && tgt.tangentB && tgt.tangentC && tgt.tangentD && tgt.bezierA && tgt.bezierB && tgt.bezierC && tgt.bezierD)
         {
             if (amountOfPrefab < 1)
             {
@@ -117,15 +134,31 @@ public class LimitEditor : Editor
 
             tgt.tangentA.position = Handles.PositionHandle(tgt.tangentA.position, tgt.tangentA.rotation);
             tgt.tangentB.position = Handles.PositionHandle(tgt.tangentB.position, tgt.tangentB.rotation);
+            tgt.tangentC.position = Handles.PositionHandle(tgt.tangentC.position, tgt.tangentC.rotation);
+            tgt.tangentD.position = Handles.PositionHandle(tgt.tangentD.position, tgt.tangentD.rotation);
+
             tgt.bezierA.position = Handles.PositionHandle(tgt.bezierA.position, tgt.bezierA.rotation);
             tgt.bezierB.position = Handles.PositionHandle(tgt.bezierB.position, tgt.bezierB.rotation);
+            tgt.bezierC.position = Handles.PositionHandle(tgt.bezierC.position, tgt.bezierC.rotation);
+            tgt.bezierD.position = Handles.PositionHandle(tgt.bezierD.position, tgt.bezierD.rotation);
 
             Handles.DrawBezier(tgt.bezierA.position, tgt.bezierB.position,
                                tgt.tangentA.position, tgt.tangentB.position,
                                Color.blue, EditorGUIUtility.whiteTexture, 2);
 
+            Handles.DrawBezier(tgt.bezierC.position, tgt.bezierD.position,
+                               tgt.tangentC.position, tgt.tangentD.position,
+                               Color.blue, EditorGUIUtility.whiteTexture, 2);
+
+            Handles.DrawLine(tgt.bezierA.position, tgt.bezierD.position);
+
+            Handles.DrawLine(tgt.bezierC.position, tgt.bezierB.position);
+
             var myPoints = Handles.MakeBezierPoints(tgt.bezierA.position, tgt.bezierB.position,
             tgt.tangentA.position, tgt.tangentB.position, amountOfPrefab);
+
+            var myPointsTwo = Handles.MakeBezierPoints(tgt.bezierC.position, tgt.bezierD.position,
+            tgt.tangentC.position, tgt.tangentD.position, amountOfPrefab);
 
             for (int i = 0; i < myPoints.Length; i++)
             {
--- Final - Motores II - Unityplus/Assets/Limits/LimitDefiner.cs
+++ Final - Motores II - Unityplus/Assets/Limits/LimitDefiner.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 
 public class LimitDefiner : MonoBehaviour
 {
-    public Transform bezierA;
+    public Transform bezierA; 
+    public Transform bezierB;   
+    public Transform bezierC;   
+    public Transform bezierD;    
+    
     public Transform tangentA;
-    public Transform bezierB;
     public Transform tangentB;
+    public Transform tangentC;
+    public Transform tangentD;
+
     public GameObject Prefab;
 }
